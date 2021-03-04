e15dd0a353849c60fcd235dae464ec15c187b37d
lxl <lxl177177@163.com>
2021-03-04 16:43:12 +0800

    1.测试python 面向对象

--- com/lxl/test/Sln_Demo.py
+++ com/lxl/test/Sln_Demo.py
@@ -1,6 +1,40 @@
 import  keyword as kw
 
-print(kw.kwlist)
-#
+# print(kw.kwlist)
+
+
+class Person:
+
+    name = ''
+    age = ''
+
+    def __init__(self):
+        self.name='张三'
+        self.age='10'
+
+    def __init__ (self,nameParam,ageParam):
+        self.name=nameParam
+        self.age=ageParam
+
+    def speak(self):
+        return "姓名是:"+self.name+";"+"年龄是:"+self.age
+
+
+if __name__ == '__main__':
+
+    # p1 = Person()
+    # p1.age='18'
+    # p1.name='bruce'
+    # print(p1.speak())
+    p2 = Person('李四','20')
+    print(p2.speak())
+
+
+
+
+
+
+
+
 
 
