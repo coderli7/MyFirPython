ac4e0b0390872ff43b0f247b7eaecc8d09965e3f
lxl <lxl177177@163.com>
2021-03-25 15:50:26 +0800

    1.numpy&pandas

---
+++ com/newtest/03-numpy&pandas/Numpy_Demo1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+# s = pd.Series([1,2,3,np.nan,44,1])
+# print(s)
+# print(type(s))
+
+
+dates = pd.date_range('20160101',periods=6)
+df = pd.DataFrame(np.random.randn(6,4),index=dates,columns=['a','b','c','d'])
+print(df)
+# print (type(df))
+
