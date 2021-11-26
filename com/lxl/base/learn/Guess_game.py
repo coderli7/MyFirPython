f608f7e832bc46844a633f356629fb12076d19f1
bruce <lxl177177@163.com>
2021-11-26 16:28:07 +0800

    hp提交测试

--- com/lxl/base/learn/Guess_game.py
+++ com/lxl/base/learn/Guess_game.py
@@ -5,16 +5,16 @@ def play():
     random_int=randint(1,100)
 
     while True:
-        user_guess=int(input("please input one number between 1 to 100:"))
+        user_guess=int(input("输入数字 between 1 to 100:"))
 
         if user_guess==random_int:
-            print("you win")
+            print("你赢了")
             break
         if user_guess<random_int:
-            print("less")
+            print("太小了")
             continue
         if user_guess>random_int:
-            print("more")
+            print("太大了")
             continue
 
 
